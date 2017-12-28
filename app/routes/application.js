ed311564818b6f38d647ce3e6c1e6221290de6d4
Carlo Gilmar <carlogilmar12@gmail.com>
2017-12-28 00:52:49 -0600

    Catching username and get a transitionTo other route

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,5 +3,13 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(){
     return this.store.findRecord('user', 'carlogilmar');
+  },
+  actions:{
+    findAdminByUser:function(){
+      var username = this.get('controller').get('username');
+      console.log(username);
+      this.get('controller').set('username', "");
+      this.transitionTo('find', username);
+    }
   }
 });
