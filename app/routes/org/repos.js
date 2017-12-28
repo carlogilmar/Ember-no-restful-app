4dd0f651291d9e42a30ad57606a081511ac5d90c
Carlo Gilmar <carlogilmar12@gmail.com>
2017-12-28 00:02:41 -0600

    Making ajax request

--- app/routes/org/repos.js
+++ app/routes/org/repos.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(){
+    return Ember.$.ajax({
+      method: 'GET',
+      url: 'http://emailerv2.modulusuno.com/show',
+    }).then((info)=>{
+      console.log("se hzo!");
+    });
+  }
 });
